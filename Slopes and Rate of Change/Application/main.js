e892d5aad70949fd198186bb0424f2ed4a9c08c5
stranius <48070684+stranius@users.noreply.github.com>
2019-02-28 10:24:24 -0600

    Delete main.js

--- Slopes and Rate of Change/Application/main.js
+++
@@ -1,5 +0,0 @@
-let graph = new Graph(10, 10); //Create a new instance of a graph with rows and columns
-graph.change_x_axis_label('Time (Seconds)', 'seconds');
-graph.change_y_axis_label('Distance (Meters)', 'meters');
-
-let sidebar = new Sidebar(graph);
